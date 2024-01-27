cec466cc1198a7c9f4735ea6a19c0f2359434ad8
colebrookson <cole.brookson@gmail.com>
2024-01-26 19:33:46 -0500

    lockfile and big model

--- _targets.R
+++ _targets.R
@@ -4,12 +4,14 @@
 
 library(targets)
 library(here)
+library(crew.cluster)
 
 source(here("./src/R/00_functions.R"))
-tar_option_set(packages = c("readr", "dplyr", "ggplot2", "rstanarm"))
+tar_option_set(packages = c("readr", "dplyr", "ggplot2", "rstanarm", "qs"))
 list(
   tar_target(file, here("./data/airquality.csv"), format = "file"),
   tar_target(data, get_data(file)),
   tar_target(model, fit_model(data)),
-  tar_target(plot, plot_model(model, data))
+  tar_target(plot, plot_model(model, data)),
+  tar_target(big, big_model(data))
 )
--- src/R/00_functions.R
+++ src/R/00_functions.R
@@ -17,3 +17,13 @@ plot_model <- function(model, data) {
     geom_point(aes(x = Temp, y = Ozone)) +
     geom_abline(intercept = model[1], slope = model[2])
 }
+
+big_model <- function(data) {
+  rstanarm::stam_lm(Ozone ~ Temp + Wind + Solar.R + Month + Day,
+    data = data,
+    iter = 10000,
+    chains = 4,
+    cores = 4
+  ) %>%
+    qs::qsave(here::here("./outputs/big_model.qs"))
+}
