f7d80b6d72975eb2202a1b3ebe5ad1cb7b02278b
colebrookson <cole.brookson@gmail.com>
2024-01-31 18:36:18 -0500

    the slurm version?

--- _targets.R
+++ _targets.R
@@ -9,23 +9,29 @@ library(crew.cluster)
 
 source(here("./src/R/00_functions.R"))
 
-controller_local <- crew::crew_controller_local(
-  name = "local_small",
+controller_local <- crew::crew_controller_slurm(
+  name = "small_slurm",
   workers = 1,
-  seconds_timeout = 60
+  seconds_timeout = 60,
+  slurm_cpus_per_task = 1,
+  workers = 1
 )
-controller_cluster <- crew::crew_controller_local(
-  name = "cluster_big",
-  workers = 2,
-  seconds_timeout = 60
+controller_cluster <- crew::crew_controller_slurm(
+  name = "bigger_slurm",
+  workers = 4,
+  seconds_timeout = 60,
+  slurm_cpus_per_task = 4,
+  slurm_memory_gigabytes_per_cpu = 20
 )
 tar_option_set(
   packages = c("readr", "dplyr", "ggplot2", "rstanarm", "qs"),
   controller = crew::crew_controller_group(controller_local,
                                            controller_cluster),
   resources = tar_resources(
-    crew = tar_resources_crew(controller = "local_small")
-  )
+    crew = tar_resources_crew(controller = "small_slurm")
+  ),
+  garbage_collection = TRUE,
+  memory = "transient"
 )
 list(
   tar_target(data, get_data(here::here("./data/airquality.csv"))),
@@ -33,7 +39,7 @@ list(
   tar_target(plot, plot_model(model, data)),
   tar_target(big, big_model(data),
              resources = tar_resources(
-               crew = tar_resources_crew(controller = "cluster_big",
+               crew = tar_resources_crew(controller = "bigger_slurm",
                                          seconds_timeout = 60)
              )),
   tar_target(plot_big, big_plot(big))
