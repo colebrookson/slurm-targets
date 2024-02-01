4871e7a0fbd7043ff239d54793021168bfb3f1e6
colebrookson <cole.brookson@gmail.com>
2024-02-01 07:59:32 -0800

    targets?

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ library(crew.cluster)
 
 source(here("./src/R/00_functions.R"))
 
-controller_local <- crew.cluster::crew_controller_slurm(
+controller_small <- crew.cluster::crew_controller_slurm(
   name = "small_slurm",
   seconds_timeout = 60,
   slurm_cpus_per_task = 1,
@@ -28,18 +28,32 @@ controller_local <- crew.cluster::crew_controller_slurm(
     "export R_LIBS=/home/brookson/scratch/.local/R/$EBVERSIONR/"
   )
 )
-controller_cluster <- crew.cluster::crew_controller_slurm(
+controller_big <- crew.cluster::crew_controller_slurm(
   name = "bigger_slurm",
   workers = 4,
   seconds_timeout = 60,
   slurm_cpus_per_task = 4,
   slurm_memory_gigabytes_per_cpu = 20,
-  slurm_time_minutes = 5
+  slurm_time_minutes = 5,
+  script_lines = c(
+    "#SBATCH --mem-per-cpu=2G",
+    "#SBATCH --mail-user=cole.brookson@gmail.com",
+    "#SBATCH --mail-type=BEGIN",
+    "#SBATCH --mail-type=END",
+    "#SBATCH --mail-type=FAIL",
+    "#SBATCH --mail-type=REQUEUE",
+    "#SBATCH --nodes=1",
+    "#SBATCH --ntasks-per-node=32",
+    "module load StdEnv/2023 gcc/9.3.0 r/4.3.1",
+    "export R_LIBS=/home/brookson/scratch/.local/R/$EBVERSIONR/"
+  )
 )
+controller <- crew::crew_controller_group(controller_small,
+                                           controller_big) 
 tar_option_set(
   packages = c("readr", "dplyr", "ggplot2", "rstanarm", "qs"),
-  controller = crew::crew_controller_group(controller_local,
-                                           controller_cluster),
+  controller = crew::crew_controller_group(controller_small,
+                                           controller_big),
   resources = tar_resources(
     crew = tar_resources_crew(controller = "small_slurm")
   ),
