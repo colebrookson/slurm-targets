bee07fed4cb58718565453fc4468d60503931f5a
colebrookson <cole.brookson@gmail.com>
2024-02-06 15:54:22 -0500

    trying with just the one controller

--- _targets.R
+++ _targets.R
@@ -11,7 +11,7 @@ source(here("./src/R/00_functions.R"))
 
 controller_small <- crew.cluster::crew_controller_slurm(
   name = "small_slurm",
-  workers = 4,
+  workers = 1,
   slurm_time_minutes = 10,
   seconds_idle = 600,
   script_lines = c(
@@ -23,40 +23,38 @@ controller_small <- crew.cluster::crew_controller_slurm(
     "#SBATCH --mail-type=FAIL",
     "#SBATCH --mail-type=REQUEUE",
     "#SBATCH --nodes=1",
-    "#SBATCH --ntasks-per-node=4",
+    "#SBATCH --ntasks-per-node=1",
     "module load StdEnv/2023 r/4.3.1"
   ),
   slurm_log_output = "/home/brookson/scratch/output.txt",
   slurm_log_error = "/home/brookson/scratch/error.txt",
 )
-controller_big <- crew.cluster::crew_controller_slurm(
-  name = "bigger_slurm",
-  workers = 4,
-  slurm_cpus_per_task = 4,
-  slurm_time_minutes = 10,
-  seconds_idle = 600,
-  script_lines = c(
-    "#SBATCH --mem-per-cpu=6G",
-    "#SBATCH --mail-user=cole.brookson@gmail.com",
-    "#SBATCH --account=def-bat3man",
-    "#SBATCH --mail-type=BEGIN",
-    "#SBATCH --mail-type=END",
-    "#SBATCH --mail-type=FAIL",
-    "#SBATCH --mail-type=REQUEUE",
-    "#SBATCH --nodes=1",
-    "#SBATCH --ntasks-per-node=4",
-    "module load StdEnv/2023 r/4.3.1"
-  ),
-  slurm_log_output = "/home/brookson/scratch/big-output.txt",
-  slurm_log_error = "/home/brookson/scratch/big-error.txt",
-)
+# controller_big <- crew.cluster::crew_controller_slurm(
+#   name = "bigger_slurm",
+#   workers = 4,
+#   slurm_cpus_per_task = 4,
+#   slurm_time_minutes = 10,
+#   seconds_idle = 600,
+#   script_lines = c(
+#     "#SBATCH --mem-per-cpu=6G",
+#     "#SBATCH --mail-user=cole.brookson@gmail.com",
+#     "#SBATCH --account=def-bat3man",
+#     "#SBATCH --mail-type=BEGIN",
+#     "#SBATCH --mail-type=END",
+#     "#SBATCH --mail-type=FAIL",
+#     "#SBATCH --mail-type=REQUEUE",
+#     "#SBATCH --nodes=1",
+#     "#SBATCH --ntasks-per-node=4",
+#     "module load StdEnv/2023 r/4.3.1"
+#   ),
+#   slurm_log_output = "/home/brookson/scratch/big-output.txt",
+#   slurm_log_error = "/home/brookson/scratch/big-error.txt",
+# )
 controller <- crew::crew_controller_group(controller_small,
                                            controller_big) 
 tar_option_set(
   packages = c("readr", "dplyr", "ggplot2", "rstanarm", "qs"),
-  controller = #crew::crew_controller_group(
-    controller_small,
-                                           #controller_big),
+  controller = controller_small,
   resources = tar_resources(
     crew = tar_resources_crew(controller = "small_slurm")
   ),
@@ -70,12 +68,12 @@ list(
   tar_target(data, get_data(here::here("./data/airquality.csv"))),
   tar_target(model, fit_model(data)),
   tar_target(plot, plot_model(model, data)),
-  tar_target(big, big_model(data)
-  #,
-            #  resources = tar_resources(
-            #    crew = tar_resources_crew(controller = "bigger_slurm",
-            #                              seconds_timeout = 60)
-            #  )
-            ),
-  tar_target(plot_big, big_plot(big))
+  # tar_target(big, big_model(data)
+  # #,
+  #           #  resources = tar_resources(
+  #           #    crew = tar_resources_crew(controller = "bigger_slurm",
+  #           #                              seconds_timeout = 60)
+  #           #  )
+  #           ),
+  # tar_target(plot_big, big_plot(big))
 )
